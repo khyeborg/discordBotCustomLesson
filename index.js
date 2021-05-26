6a81e5ccb3b90dd79accd227a5d0dd9d686dd564
Khye Borg Liew <51248004+khyeborg@users.noreply.github.com>
2021-05-26 00:56:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,15 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
 const client = new Discord.Client();
-
 // Run handleMessage() whenever a new message sent
 client.on("message", handleMessage);
-
 // Replace with your own initials!
 const prefix = "!NM";
-
 function handleMessage(message) {
     // Check if a bot sent the message
     if (message.author.bot == true) return;
-    
     // Check that message starts with our prefix
     if (message.content.startsWith(prefix) == false) return;
-
     // Add your own code HERE!
-
 }
-
 client.login(config.token);
